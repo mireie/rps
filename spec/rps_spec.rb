11b2c3d8390159a12bd3f4bac6289ead10716071
mireie <michael.reiersgaard@gmail.com>
2021-07-14 10:44:49 -0700

    add script. all tests pass
    Co-Authoered-By: Kyle Williamson <kylewilliamsonv1@gmail.com>

--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -7,4 +7,12 @@ describe('#title_case') do
     game = RPS.new()
     expect(game.wins?("rock", "scissors")).to(eq(true))
   end
+  it("returns true if scissors is the object and scissors is the argument") do
+    game = RPS.new()
+    expect(game.wins?("scissors", "rock")).to(eq(false))
+  end
+  it("returns draw if p1 and p2 are the same") do
+    game = RPS.new()
+    expect(game.wins?("rock", "rock")).to(eq("draw"))
+  end
 end
